fbd2112a817728c418d4cfd67ceda74495de197a
olga <olga.lescuyer@gmail.com>
2024-04-18 15:37:01 +0200

    add: footer & onScroll animation

--- app/layout.js
+++ app/layout.js
@@ -2,7 +2,7 @@ import './globals.css';
 import { primary } from '@/app/ui/fonts/fonts';
 
 import { AppWrapper } from '@/app/providers/AppWrapper';
-import { Header, LatestNews } from '@/app/ui/ui';
+import { Header, LatestNews, Footer, MoveOnScroll } from '@/app/ui/ui';
 
 export const metadata = {
   title: 'Create Next App',
@@ -16,13 +16,17 @@ export default function RootLayout({ children }) {
         <AppWrapper>
           {' '}
           <div className=" min-h-screen ">
-            <Header />
+            <MoveOnScroll>
+              <Header />
+            </MoveOnScroll>
+
             <main>
               <div className="main-container mt-40 lg:grid lg:grid-cols-12 gap-6">
                 {children}
                 {/* <LatestNews /> */}
               </div>
             </main>
+            <Footer />
           </div>
         </AppWrapper>
       </body>
